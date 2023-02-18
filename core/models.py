a98d295f5ff31d9a3bb8c5e1a9208228cf7a4b81
Senimtra <senimtra@hotmail.com>
2023-02-18 12:38:43 +0100

    create database models (commodities + auctions)

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+# Commodities database model
+class Commodities(models.Model):
+   item_id = models.IntegerField()
+   quantity = models.IntegerField()
+   unit_price = models.IntegerField()
+   weighted = models.IntegerField()
+   timestamp = models.DateTimeField()
+
+# Auctions database model
+class Auctions(models.Model):
+   item_id = models.IntegerField()
+   quantity = models.IntegerField()
+   unit_price = models.IntegerField()
+   timestamp = models.DateTimeField()
