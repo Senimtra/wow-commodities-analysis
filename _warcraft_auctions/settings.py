b27a71f5bda5b6b7ab7c35e3f4b52559ecca0331
Senimtra <senimtra@hotmail.com>
2023-02-20 21:49:23 +0100

    create 404 view

--- _warcraft_auctions/settings.py
+++ _warcraft_auctions/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(5v+9y^uktq0(d6k^i^$)=lq!#9hpfl*u)s$w&u&@gg)0%^__)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,7 +117,8 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = ''
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
