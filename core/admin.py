4892aeceda21220194927c2331e1e542e7859f9c
Senimtra <senimtra@hotmail.com>
2023-02-20 21:54:47 +0100

    configure admin panel (commodities + auctions)

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Commodities, Auctions
 
-# Register your models here.
+class CommoditiesAdmin(admin.ModelAdmin):
+    list_display = ('item_id', 'quantity', 'unit_price', 'weighted', 'timestamp')
+
+class AuctionsAdmin(admin.ModelAdmin):
+    list_display = ('item_id', 'quantity', 'unit_price', 'timestamp')
+
+admin.site.register(Commodities, CommoditiesAdmin)
+admin.site.register(Auctions, AuctionsAdmin)
