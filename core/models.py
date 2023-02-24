db1fde61881db723de7e13752af0726fea7d0409
Senimtra <senimtra@hotmail.com>
2023-02-24 21:17:57 +0100

    add bootstrap + navbar

--- core/models.py
+++ core/models.py
@@ -8,9 +8,15 @@ class Commodities(models.Model):
    weighted = models.IntegerField()
    timestamp = models.DateTimeField()
 
+   class Meta:
+      verbose_name_plural = 'commodities'
+
 # Auctions table model
 class Auctions(models.Model):
    item_id = models.IntegerField()
    quantity = models.IntegerField()
    unit_price = models.IntegerField()
    timestamp = models.DateTimeField()
+
+   class Meta:
+      verbose_name_plural = 'auctions'
