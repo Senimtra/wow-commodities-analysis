afb9a211ea7e1764606acdc698cbcb4c69ace253
Senimtra <senimtra@hotmail.com>
2023-02-25 23:49:23 +0100

    create database models (items data + media)

--- core/models.py
+++ core/models.py
@@ -20,3 +20,19 @@ class Auctions(models.Model):
 
    class Meta:
       verbose_name_plural = 'auctions'
+
+# Items data table model
+class ItemsData(models.Model):
+   item_id = models.IntegerField()
+   name = models.CharField(max_length = 100)
+   quality = models.CharField(max_length = 20)
+   level = models.IntegerField()
+   item_class = models.CharField(max_length = 30)
+   item_subclass = models.CharField(max_length = 30)
+   description = models.TextField(max_length = 500)
+
+# Items media table model
+class ItemsMedia(models.Model):
+   item_id = models.IntegerField()
+   media = models.CharField(max_length = 20)
+   link = models.CharField(max_length = 255)
